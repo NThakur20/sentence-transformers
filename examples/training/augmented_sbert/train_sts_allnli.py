82197b6a0328fbecb7171d0458e114cead0a0b0a
Nthakur20 <nandant@gmail.com>
2020-10-06 18:54:05 +0200

    added training of cross-encoder over sts model

--- examples/training/augmented_sbert/train_sts_allnli.py
+++ examples/training/augmented_sbert/train_sts_allnli.py
@@ -18,16 +18,25 @@ OR
 python train_sts_allnli.py pretrained_transformer_model_name
 """
 from torch.utils.data import DataLoader
-import math
 from simpletransformers.classification import ClassificationModel, ClassificationArgs
 from sentence_transformers import models, losses
 from sentence_transformers import SentencesDataset, LoggingHandler, SentenceTransformer
 from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
 from sentence_transformers.readers import *
-import logging
 from datetime import datetime
+from scipy.stats import pearsonr, spearmanr
+import pandas as pd
+import logging
 import scipy.spatial
 import sys
+import torch
+import math
+
+def pearson_corr(preds, labels):
+    return pearsonr(preds, labels)[0]
+
+def spearman_corr(preds, labels):
+    return spearmanr(preds, labels)[0]
 
 #### Just some code to print debug information to stdout
 logging.basicConfig(format='%(asctime)s - %(message)s',
@@ -38,12 +47,14 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
 
 #You can specify any huggingface/transformers pre-trained model here, for example, bert-base-uncased, roberta-base, xlm-roberta-base
 model_name = sys.argv[1] if len(sys.argv) > 1 else 'bert-base-uncased'
-
-###### Read Datasets ######
 batch_size = 16
 num_epochs = 1
-nli_reader = NLIDataReader('../datasets/AllNLI')
-sts_reader = STSBenchmarkDataReader('../datasets/stsbenchmark', normalize_scores=True)
+max_seq_length = 128
+use_cuda = torch.cuda.is_available()
+
+###### Read Datasets ######
+nli_reader = NLIDataReader('../../datasets/AllNLI')
+sts_reader = STSBenchmarkDataReader('../../datasets/stsbenchmark', normalize_scores=True)
 cross_encoder_model_save_path = 'output/cross_encoder/sts_allnli_'+model_name.replace("/", "-")+'-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 aug_sbert_model_save_path = 'output/augmented_sbert/sts_allnli_'+model_name.replace("/", "-")+'-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
@@ -51,21 +62,24 @@ aug_sbert_model_save_path = 'output/augmented_sbert/sts_allnli_'+model_name.repl
 ###### Cross-encoder (simpletransformers) ######
 # Enabling regression
 # Setting optional model configuration
-logging.info("Loading cross-encoder: {} model.....".format(model_name))
+logging.info("Loading cross-encoder model: {}".format(model_name))
 model_args = ClassificationArgs()
+
 
 # Enabling regression
 # Setting optional model configuration
 model_args.num_train_epochs = num_epochs
 model_args.regression = True
+model_args.max_seq_length = max_seq_length # by-default in cross-encoder its 512
+model_args.train_batch_size = batch_size
 
-cross_encoder_model = ClassificationModel("bert", model_name)
+cross_encoder_model = ClassificationModel(model_name.split("-")[0], model_name, num_labels=1, use_cuda=use_cuda)
 
 
 ###### Bi-encoder (sentence-transformers) ######
-logging.info("Loading cross-encoder: {} model.....".format(model_name))
+logging.info("Loading bi-encoder model: {}".format(model_name))
 # Use Huggingface/transformers model (like BERT, RoBERTa, XLNet, XLM-R) for mapping tokens to embeddings
-word_embedding_model = models.Transformer(model_name)
+word_embedding_model = models.Transformer(model_name, max_seq_length=max_seq_length)
 
 # Apply mean pooling to get one fixed sized sentence vector
 pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(),
@@ -74,40 +88,40 @@ pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension
                                pooling_mode_max_tokens=False)
 
 bi_encoder_model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+
+
 #####################################################
 #
 # Step 1: Train cross-encoder model with STSbenchmark
 #
 #####################################################
-
 logging.info("Step 1: Train cross-encoder: ({}) with STSbenchmark".format(model_name))
 
 # Convert the dataset to a DataLoader ready for training
 logging.info("Read STSbenchmark train dataset")
-train_data = SentencesDataset(sts_reader.get_examples('sts-train.csv'), cross_encoder_model)
-train_dataloader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
-train_loss = losses.CosineSimilarityLoss(model=cross_encoder_model)
+
+train_data = []
+for example in sts_reader.get_examples('sts-train.csv'):
+    train_data.append(example.texts + [example.label])
+
+train_df = pd.DataFrame(train_data, columns=['text_a', 'text_b', 'labels'])
 
 # Convert the dataset to a DataLoader ready for dev
 logging.info("Read STSbenchmark dev dataset")
-dev_data = SentencesDataset(examples=sts_reader.get_examples('sts-dev.csv'), model=cross_encoder_model)
-dev_dataloader = DataLoader(dev_data, shuffle=False, batch_size=batch_size)
-evaluator = EmbeddingSimilarityEvaluator(dev_dataloader, name='cross-encoder')
 
+eval_data = []
+for example in sts_reader.get_examples('sts-dev.csv'):
+    eval_data.append(example.texts + [example.label])
 
-# Configure the training.
-warmup_steps = math.ceil(len(train_data)*num_epochs/batch_size*0.1) #10% of train data for warm-up
-logging.info("Warmup-steps: {}".format(warmup_steps))
-
+eval_df = pd.DataFrame(eval_data, columns=['text_a', 'text_b', 'labels'])
 
 # Train the cross-encoder model
-cross_encoder_model.fit(train_objectives=[(train_dataloader, train_loss)],
-          evaluator=evaluator,
-          epochs=num_epochs,
-          evaluation_steps=1000,
-          warmup_steps=warmup_steps, 
-          output_path=cross_encoder_model_save_path)
+cross_encoder_model.train_model(train_df, eval_df=eval_df, pearson_corr=pearson_corr, spearman_corr=spearman_corr)
 
+# Evaluate the cross-encoder model
+result, model_outputs, wrong_predictions = model.eval_model(eval_df, pearson_corr=pearson_corr, spearman_corr=spearman_corr)
+
+print(result, model_outputs)
 
 ########################################################
 #
